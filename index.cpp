f675c3f788425c2ad15b2a42aa0049dbd3b2c765
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2022-10-31 02:14:01 +0500

    data types added

--- index.cpp
+++ index.cpp
@@ -12,6 +12,28 @@ int main(int argc, char** argv)
     cout << "Hello World!"<< endl;
 
     // here I type the data types in cpp
+    bool married = true;
+    char myGrade = 'A';
+
+    cout << "Min unsighned int" <<
+        numeric_limits<u_int16_t>::min()
+        << endl;
+    
+    cout << "Max unsighned int" <<
+        numeric_limits<u_int16_t>::max()
+        << endl;
+    
+    cout << "Min float "<< 
+        numeric_limits<float>::min() 
+        << endl;
+
+    cout << "Max float"<<
+        numeric_limits<float>::max()
+        << endl;
+        
+    cout << "Min double"<<
+        numeric_limits<double>::min()
+        << endl;
 
     return 0;
 }
